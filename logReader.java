5926a37f0b4d820c1f5de74172c02fb8daffef5b
QuinnDell <77847418+QuinnDell@users.noreply.github.com>
2021-03-08 20:02:28 -0600

    Update logReader.java

--- logReader.java
+++ logReader.java
@@ -7,23 +7,22 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class logReader {
-	
+
 		
 public static void main(String[] args) {    
-	try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\quinn\\OneDrive\\Desktop\\Samba File Server Log.csv"))){ // add log here
+	try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\quinn\\OneDrive\\Desktop\\sysLog.log.txt"))){ // add log here
 		 
-		BufferedWriter out = new BufferedWriter(new FileWriter("apeandedLog.log")); // Changer log to what we want to be recorded
+		BufferedWriter out = new BufferedWriter(new FileWriter("SystemLog.csv")); // Changer log to what we want to be recorded
 		   
 		   String strLine;
 
 		   // read log line by line
-		   while ((strLine = br.readLine()) != null)   {	
-			   String[] split = strLine.split(",");
+		   while ((strLine = br.readLine()) != null)   {				   
 			   
-		     // parse strLine to obtain what you want
-		     System.out.print(split[0] + " ");		     
-		     
-		     out.write(split[0]+ " ");
+			   String csv = strLine.replace(",", ";"); 
+			   
+		     // parse strLine to obtain what you want		     
+		     out.write(csv + ",\n");
 		     		     
 
 		   }
