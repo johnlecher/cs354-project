753cf8df76fb986c0f3407a8d59873c557efa865
Jay <johnlecher117@gmail.com>
2021-03-09 22:31:20 -0600

    Removed deprecated logReader and renamed new logReader correctly

--- logReader.java
+++ logReader.java
@@ -1,5 +1,3 @@
-package intru;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -8,29 +6,25 @@ import java.io.IOException;
 
 public class logReader {
 
-		
 public static void main(String[] args) {    
-	try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\quinn\\OneDrive\\Desktop\\sysLog.log.txt"))){ // add log here
-		 
-		BufferedWriter out = new BufferedWriter(new FileWriter("SystemLog.csv")); // Changer log to what we want to be recorded
-		   
-		   String strLine;
-
-		   // read log line by line
-		   while ((strLine = br.readLine()) != null)   {				   
-			   
-			   String csv = strLine.replace(",", ";"); 
-			   
-		     // parse strLine to obtain what you want		     
-		     out.write(csv + ",\n");
-		     		     
-
-		   }
-		  out.close();
-		   
-		} catch (IOException e) {
-		      e.printStackTrace();
-		  }
-	
-}
+    String fileLoc = "FILE LOCATION HERE"; //Enter log file location
+    try (BufferedReader br = new BufferedReader(new FileReader(fileLoc))){
+        
+        BufferedWriter out = new BufferedWriter(new FileWriter("src/SystemLog.csv"));
+        String strLine = br.readLine(); // read log line by line 
+        String[] strSplit;
+        while(strLine != null) {
+            strSplit = strLine.split(",");
+            for(int i = 0; i<strSplit.length; i++) {
+                out.write(strSplit[i]+",");
+                }
+            out.write("\n");
+            strLine = br.readLine();
+            }
+        out.close();
+           
+        } catch (IOException e) {
+              e.printStackTrace();
+        }
+    }
 }
