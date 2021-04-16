a8b362cfb4cccb0e03af41de74e605ebbda41e77
chriad14 <80172714+chriad14@users.noreply.github.com>
2021-04-15 21:55:15 -0500

    Update appLog.java

--- appLog.java
+++ appLog.java
@@ -8,7 +8,7 @@ public class appLog {
 
 		
 public static void main(String[] args) {    
-	String fileLoc = "FILE LOCATION HERE";
+	String fileLoc = "C:\\Users\\Chriad\\Downloads\\Samba File Server Log(1).csv";
 	try (BufferedReader br = new BufferedReader(new FileReader(fileLoc))){ // add log here
 		
 		BufferedWriter out = new BufferedWriter(new FileWriter("src/ApplicationLog.csv"));
@@ -34,10 +34,18 @@ public static void main(String[] args) {
 		strLine = br.readLine(); // read log line by line
 		int line = 2;
         String[] strSplit;
+        int count = 0;
         while(strLine != null) {
         	strSplit = strLine.split(",");
         	if(strSplit[4].equalsIgnoreCase("EMPTY")) {
-        		System.out.println("Error located in log line: " + line);
+        		System.out.println("Login failure located in log line: " + line);
+        		count++;
+        		if(count==3) {
+        			count = 0;
+        			System.out.println();
+        			System.out.println("ALERT: Three failed login atempts detected. Administrator inspection recommended.");
+        			System.out.println();
+        		}
         	}
         	strLine = br.readLine();
         	line++;
