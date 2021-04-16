b496a75a975b5971d0c83581ddbc3567d5b594d6
chriad14 <80172714+chriad14@users.noreply.github.com>
2021-04-16 18:18:06 -0500

    Update dataAgg.java

--- dataAgg.java
+++ dataAgg.java
@@ -1,20 +1,41 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class dataAgg {
+public class dataAgg2 {
 
 	public static void main(String[] args) {
-		try(BufferedReader br = new BufferedReader(new FileReader("src/ApplicationLog.csv"))) {
+		
+		ArrayList<String> logs = new ArrayList<String>();
+		logs.add("src/log.csv");
+		logs.add("src/ApplicationLog.csv");
+		System.out.println("Library: " + logs.toString());
+		Scanner fileScan = new Scanner(System.in);
+		String loc;
+		do {
+			System.out.print("\nEnter a file location of a log that needs to be added to the library (else enter 'x'): ");
+			loc = fileScan.next();
+			if(loc.equalsIgnoreCase("x"))
+				break;
+			logs.add(loc);
+			System.out.println("\nLibrary: " + logs.toString());
+		} while(loc != "");
+		
+		System.out.print("\n\nEnter the index of which file to search: ");
+		int i1 = fileScan.nextInt();
+		
+		String location = logs.get(i1);
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(location))) {
 			
 			String strLine = br.readLine();
 			strLine = br.readLine(); // read log line by line
 			int line = 2;
 	        String[] strSplit;
-	        Scanner term = new Scanner(System.in);
 	        System.out.print("Enter what term to search for: ");
-	        String search = term.nextLine();
+	        String search = fileScan.next();
 	        System.out.println("Searching for: " + "[" +search+ "]");
 	        while(strLine != null) {
 	        	strSplit = strLine.split(",");
@@ -28,6 +49,7 @@ public class dataAgg {
 	        }
 		} catch (IOException e) {
 			e.printStackTrace();
-			}
+		}
+		fileScan.close();
 	}
 }
